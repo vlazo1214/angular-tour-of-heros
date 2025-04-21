7b5daca7fd353717879b410aefcac7f75cac97f4
vincent.lazo <vincent.lazo@ucf.edu>
2025-04-21 13:59:42 -0400

    up to third module

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Groot' },
+    { id: 13, name: 'Thor' },
+    { id: 14, name: 'Storm' },
+    { id: 15, name: 'Magneto' },
+    { id: 16, name: 'Mr. Fantastic' },
+    { id: 17, name: 'Iron Fist' },
+    { id: 18, name: 'Human Torch' },
+    { id: 19, name: 'Luna Snow' },
+    { id: 20, name: 'Squirrel Girl' }
+];
