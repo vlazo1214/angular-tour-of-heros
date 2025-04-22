56cac508c6cf49b47ff1701d3fcd3837f00be524
vincent.lazo <vincent.lazo@ucf.edu>
2025-04-22 14:42:11 -0400

    added routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,18 +11,12 @@ import { MessagesService } from '../messages.service';
 })
 export class HeroesComponent implements OnInit{
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService,
               private messageService: MessagesService) {}
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
