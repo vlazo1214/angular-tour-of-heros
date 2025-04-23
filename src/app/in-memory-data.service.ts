0c8a87cfd99a5605c7f86f45b9549b343441ad7b
vincent.lazo <vincent.lazo@ucf.edu>
2025-04-23 13:42:37 -0400

    added crud functionality with mock server

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 1, name: 'Black Panther' },
+      { id: 2, name: 'Hulk' },
+      { id: 3, name: 'Jeff the LandShark' },
+      { id: 4, name: 'Cloak & Dagger' },
+      { id: 5, name: 'Magik' },
+      { id: 6, name: 'Scarlet Witch' },
+      { id: 7, name: 'Emma Frost' },
+      { id: 8, name: 'Psylocke' },
+      { id: 9, name: 'Invisible Woman' }
+    ];
+    return {heroes};
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
